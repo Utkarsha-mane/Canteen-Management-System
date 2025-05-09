12665be2444e3c7c3389cffb01993ce916238297
mahik9754 <mahi.khare456@gmail.com>
2025-05-09 14:46:35 +0530

    Update MenuCard.java

--- MenuCard.java
+++ MenuCard.java
@@ -3,6 +3,7 @@ package mini;
 public abstract class MenuCard {
 	String name;
 	double price;
+	int order_count;
 	public MenuCard(String name, double price) {
 	this.name = name;
 	this.price = price;
