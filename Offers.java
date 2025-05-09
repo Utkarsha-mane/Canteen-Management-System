34e4e48f852d4ab9be85749dfa9525b94182941e
Arya Dixit <arya.dixit@cumminscollege.in>
2025-05-09 14:58:23 +0530

    Update Offers.java

--- Offers.java
+++ Offers.java
@@ -1,5 +1,13 @@
 package mini;
-
+import java .util.*;
 public class Offers {
+  
+ void showOffers(){
+
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Would you like to view the offers we are currently providing?");
+}
+
+
 
 }
