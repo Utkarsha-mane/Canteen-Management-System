d79ec03d35ecf8086cdd26f17e4ba750aa219fdd
Arya Dixit <arya.dixit@cumminscollege.in>
2025-05-11 19:42:52 +0530

    Update Offers.java

--- Offers.java
+++ Offers.java
@@ -1,13 +1,20 @@
 package mini;
 import java .util.*;
 public class Offers {
+	ArrayList<MenuCard> offers = new ArrayList<>();
+	offers.add(new Combos("South Indian Sunrise - Idli + Upma + Hot Coffee", 60, 5 ));
+	offers.add(new Combos("Mumbai Snack Box - Vada Paav + Chutney Sandwhich", 40, 5));
+	offers.add(new Combos("Summer Juices Combo - Pick any two juices", 100, 20));
   
  void showOffers(){
 
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Would you like to view the offers we are currently providing?");
-}
-
-
+	
 
 }
+
+
+
+}
+
