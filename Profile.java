7fe31d09cf7b2ac40d4e419150d058ca129012a5
Utkarsha-mane <utkarshamane144@gmail.com>
2025-05-12 06:21:13 +0530

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,5 +1,36 @@
 package mini;
 
-public class Profile {
-     ArrayList<Profile> customers = new ArrayList<>();
+class Profile {
+	String name, email;
+	private String password;
+	int user_pin;
+	int dish_count = 0;
+	ArrayList<String> reward_history = new ArrayList<>();
+	Scanner input = new Scanner(System.in);
+
+	Profile() {
+	};
+
+	Profile(String name, String email, String password, int pin) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		user_pin = pin;
+	}
+
+	void detail(ArrayList customers) {
+		System.out.print("Enter your name: ");
+		name = input.nextLine();
+		System.out.print("Enter your email: ");
+		email = input.nextLine();
+		System.out.print("Enter your password: ");
+		password = input.nextLine();
+		System.out.print("Enter your account pin: ");
+		user_pin = input.nextInt();
+		customers.add(new Profile(name, email, password, user_pin));
+	}
+
+	int getPin() {
+		return user_pin;
+	}
 }
