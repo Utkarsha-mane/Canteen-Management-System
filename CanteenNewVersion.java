2b9cb99c6a3b42d24f222435bc09832719f86329
Utkarsha-mane <utkarshamane144@gmail.com>
2025-05-13 21:09:37 +0530

    Update CanteenNewVersion.java

--- CanteenNewVersion.java
+++ CanteenNewVersion.java
@@ -3,12 +3,21 @@ package canteen_management_system;
 import java.util.*;
 
 public class Canteen {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner input = new Scanner(System.in);
 		MenuItems m = new MenuItems();
 		ArrayList<MenuCard> mList = new ArrayList<MenuCard>();
 		ArrayList<Staff> staff_list = new ArrayList<Staff>();
 		ArrayList<Profile> customer = new ArrayList<>();
+		Profile R = new Profile();
+		Order order = new Order(R);
+		Recommendations pd = new Recommendations();
+		Feedback fd = new Feedback();
+		Random_dish Rd = new Random_dish(R, mList);
+		Prize_coupons pc = new Prize_coupons(R);
+		DishOfTheWeek dw = new DishOfTheWeek();
+		int choice;
+		String continue_ = "yes";
 		Staff admin = new Staff("admin.cumminscanteen.in", "admin123");
 		char Logout = 'N', cont, cont2;
 		int Menu = 0;
@@ -16,7 +25,7 @@ public class Canteen {
 		m.to_add_itemlist(mList);
 		do {
 			System.out.println("Welcome to the Canteen!");
-			System.out.println("1. For staff\n2. For customers");
+			System.out.println("1. For staff\n2. For customers\n3. Explore");
 			Menu = input.nextInt();
 			switch (Menu) {
 			case 1:
@@ -48,15 +57,7 @@ public class Canteen {
 				} while (Logout == 'Y');
 				break;
 			case 2:
-				Profile R = new Profile();
 				R.detail(customer);
-				Order order = new Order(R);
-				Recommendations pd = new Recommendations();
-				Feedback fd = new Feedback();
-				Random_dish Rd = new Random_dish();
-				Prize_coupons pc = new Prize_coupons(R);
-				int choice;
-				String continue_ = "yes";
 				do
 				{
 					System.out.println("Enter your choice\n1. Menucard\n2. Offers\n3. Popular Dishes\n4. Get Recommendation\n5. I'm so confused..Get me a random dish\n6. Redeem free dish\n7. Feedback");
@@ -69,7 +70,45 @@ public class Canteen {
 							// offers
 							break;
 						case 3:
-							// popular dishes
+							dw.menu = mList;
+							dw.Timer();	
+							break;
+						case 4:
+							pd.get_recommendations(R);
+							break;
+						case 5:
+							Rd.random_dish();
+							break;
+						case 6:
+							pc.redeem_points();
+							break;
+						case 7:
+							Staff.addFeedback();
+							break;
+						default:
+							System.out.println("Invalid choice!");
+					}
+					System.out.println("Show menu?");
+					continue_ = input.next();
+				}while(continue_.toLowerCase().equals("yes"));
+				break;
+				
+			case 3 :
+				System.out.println("You can pay only in cash!");
+				do
+				{
+					System.out.println("Enter your choice\n1. Menucard\n2. Offers\n3. Popular Dishes\n4. Get Recommendation\n5. I'm so confused..Get me a random dish\n6. Redeem free dish\n7. Feedback");
+					choice = input.nextInt();
+					switch (choice) {
+						case 1:
+							order.takeOrder();
+							break;
+						case 2: 
+							// offers
+							break;
+						case 3:
+							dw.menu = mList;
+							dw.Timer();	
 							break;
 						case 4:
 							pd.get_recommendations(R);
@@ -130,19 +169,20 @@ class Profile
 		 return user_pin;
 	 }	
 }
-class Prize_coupons {
+class Prize_coupons extends DishOfTheWeek{
     Profile user;
     Recommendations R_free = new Recommendations();
+    MenuItems m = new MenuItems();
     Order O_free = new Order();
-    Popular_dishes P_free = new Popular_dishes();
-    Random_dish S_free = new Random_dish();
+    DishOfTheWeek P_free = new DishOfTheWeek();
+    Random_dish S_free = new Random_dish(user, menu);
     Scanner sc = new Scanner(System.in);
     String continue_ , recom;
     Prize_coupons(Profile user) {
         this.user = user;
     }
 
-    void redeem_points() {
+    void redeem_points() throws InterruptedException {
         System.out.println("🎁 Reward Zone: Hello " + user.name + "!");
 
         if (user.dish_count >= 3) {
@@ -162,8 +202,7 @@ class Prize_coupons {
                     R_free.get_recommendations(user);
                     break;
                 case 2:
-                    P_free.get_popular_dishes();
-                    P_free.select_popular_dish();
+                    P_free.Timer();
                     break;
                 case 3:
                     S_free.random_dish();
@@ -567,42 +606,97 @@ class Order {
 }
 
 class Random_dish extends MenuItems {
-	Scanner input = new Scanner(System.in);
-	Random rand = new Random();
-	Profile R;
-	int order_or_not;
-	Order Order_random = new Order(R);
-	ArrayList<MenuCard> mList = new ArrayList<MenuCard>();
+    Scanner input = new Scanner(System.in);
+    Random rand = new Random();
+    Profile R;
+    int order_or_not;
+    Order Order_random;
+    ArrayList<MenuCard> mList;
 
-	void random_dish() {
-		if (mList.isEmpty()) {
-			System.out.println("No items available to select a surprise dish!");
-			return;
-		}
-		int randomIndex = rand.nextInt(mList.size());
-		MenuCard surpriseDish = mList.get(randomIndex);
-		System.out.println("Surprise Dish for you: " + surpriseDish.name + " - ₹" + surpriseDish.price + "\n"
-				+ "Enjoy the food. To order this dish please enter 1 :");
-		order_or_not = input.nextInt();
-		if (order_or_not == 1) {
-			Order_random.userOrders.add(surpriseDish);
-			Order_random.finalBill();
+    // Constructor to pass Profile and Menu list
+    Random_dish(Profile R, ArrayList<MenuCard> menuList) {
+        this.R = R;
+        this.Order_random = new Order(R);
+        this.mList = menuList;
+    }
 
-		}
-	}
+    void Timer() {
+        System.out.print("Choosing your surprise dish");
+        try {
+            for (int i = 0; i < 3; i++) {
+                Thread.sleep(700);
+                System.out.print(" ⏳");
+            }
+            System.out.println();
+        } catch (InterruptedException e) {
+            System.out.println("Oops! Something went wrong during the timer ⛔");
+        }
+    }
+
+    void random_dish() {
+        if (mList == null || mList.isEmpty()) {
+            System.out.println("❌ No items available to select a surprise dish!");
+            return;
+        }
+
+        Timer();
+
+        int randomIndex = rand.nextInt(mList.size());
+        MenuCard surpriseDish = mList.get(randomIndex);
+
+        System.out.println("\n🎁 Surprise Dish Just for You 🍽️");
+        System.out.println("👉 " + surpriseDish.name + " - ₹" + surpriseDish.price);
+        System.out.println("😋 Enjoy the food vibes!");
+        System.out.print("🛒 Would you like to order this dish? Enter 1 for YES: ");
+
+        order_or_not = input.nextInt();
+        if (order_or_not == 1) {
+            Order_random.userOrders.add(surpriseDish);
+            System.out.println("✅ Dish added to your order! Finalizing bill...");
+            Order_random.finalBill();
+        } else {
+            System.out.println("👌 No worries! Maybe next time. 😊");
+        }
+    }
 }
+
 
 
 class Offers {
 }
 
-class Popular_dishes {
-	void get_popular_dishes()
-	{
-		
-	}
-	void select_popular_dish()
-	{
-		
-	}
-}
+class DishOfTheWeek extends MenuItems {
+//    ArrayList<MenuCard> menu;
+//    public DishOfTheWeek(ArrayList<MenuCard> menu) {
+//        this.menu = menu;
+//    }
+	MenuItems menu_function = new MenuItems();
+	ArrayList<MenuCard> menu = new ArrayList<>();
+	
+
+    public void Timer() throws InterruptedException {
+    	menu_function.to_add_itemlist(menu);
+        System.out.println("\nChecking for the most trending dish of the week...");
+        for (int i = 5; i > 0; i--) {
+            System.out.print(i + " ");
+            Thread.sleep(1000);
+        }
+        System.out.println();
+
+        MenuCard TrendingDish = null;
+        for (MenuCard item : menu) {
+            if (item.order_count >= 3) {
+                if (TrendingDish == null || item.order_count > TrendingDish.order_count) {
+                    TrendingDish = item;
+                }
+            }
+        }
+
+        if (TrendingDish != null) {
+            System.out.println("🔥 Dish of the Week: " + TrendingDish.name + " - ₹" + TrendingDish.price + " 🔥");
+        } else {
+            System.out.println("No dish has been ordered enough times to qualify as Dish of the Week.");
+        }
+    }
+
+    }
